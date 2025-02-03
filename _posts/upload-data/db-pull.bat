d88c095140fd4de6bdf3105a1862b126db43745f
inundata <james.minho.lee@gmail.com>
2025-02-04 08:48:16 +0900

    자동 업데이트: 2025-02-04  8:48:15.90

--- _posts/upload-data/db-pull.bat
+++ _posts/upload-data/db-pull.bat
@@ -7,6 +7,7 @@ REM 2️.run git
 cd /d "E:\OneDrive\Github\[i-MAES]\[Collaborate]\[Workspace]\000. github_page\Inundata.github.io"
 git add .
 git commit -m "자동 업데이트: %date% %time%"
+git config --global credential.credentialStore dpapi
 git push -u origin main
 
 echo 작업 완료!

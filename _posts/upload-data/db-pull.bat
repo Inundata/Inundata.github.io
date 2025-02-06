0acf8fa9521d48ca685cb39bd020a98be9bf9765
inundata <james.minho.lee@gmail.com>
2025-02-06 10:56:02 +0900

    자동 업데이트: 2025-02-06 10:56:02.45

--- _posts/upload-data/db-pull.bat
+++ _posts/upload-data/db-pull.bat
@@ -1,13 +1,15 @@
 @echo off
 chcp 65001 >nul
 
-REM 현재 날짜 및 어제 날짜 계산
+REM 현재 날짜 가져오기
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| find "LocalDateTime"') do set datetime=%%I
 set today=%datetime:~2,6%
-set yesterday=
 
-REM 어제 날짜 계산 (Windows에서는 간단한 방법이 없으므로 PowerShell 사용)
+REM 어제 날짜 가져오기 (PowerShell 사용)
 for /f %%I in ('powershell -command "& {(Get-Date).AddDays(-1).ToString('yyMMdd')}"') do set yesterday=%%I
+
+echo 오늘 날짜: %today%
+echo 어제 날짜: %yesterday%
 
 REM 1. Run Python 스크립트
 "C:\Users\HERO\anaconda3\envs\db\python.exe" "E:\OneDrive\Github\inundata.github.io\_posts\upload-data\db-pull.py"
@@ -27,6 +29,7 @@ REM 변경사항 반영
 git add .
 git commit -m "자동 업데이트: %date% %time%"
 
+
 REM Git LFS에서 사용되지 않는 파일 정리
 echo y | git lfs prune
 
@@ -35,7 +38,7 @@ echo y | git reflog expire --expire=now --all
 echo y | git gc --prune=now
 
 REM GitHub로 Push
-@REM git config --global credential.credentialStore dpapi
+git config --global credential.credentialStore dpapi
 git push -u origin main
 
 echo 작업 완료!
